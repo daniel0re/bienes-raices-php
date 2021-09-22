3132bcb90c95c9afc0071d037f67f66d00aac87e
Daniel <daniel0rec98@gmail.com>
2021-09-21 23:13:20 -0500

    Agregando Cerrar-sesion.php

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -39,7 +39,7 @@
                     <a href="/blog.php">Blog</a>
                     <a href="/contacto.php">Contacto</a>
                     <?php if($auth): ?>
-                        <a href="cerrar-sesion.php">Cerrar sesion</a>"
+                        <a href="/cerrar-sesion.php">Cerrar sesion</a>"
                     <?php endif; ?>
                 </nav>
 
